98f2c1458443920ed06c34f1cc67c2bfa233c3d2
alice-hawthorne <alesya.lagu@gmail.com>
2019-09-28 10:45:08 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = str.split('');
+  let newarr = [];
+  let open = [];
+  let close = [];
+
+for (let elem of bracketsConfig) {
+  if (elem.includes('|') || elem.includes('7') || elem.includes('8')) {
+    close.push(elem[0], elem[1]);
+  } else {
+    open.push(elem[0]);
+    close.push(elem[1]);
+  }
+}
+
+for (let item of arr) {
+  if (open.includes(item)) {
+    newarr.push(item);
+  };
+  if (close.includes(item)) {
+    let pair = open[close.indexOf(item)];
+    if (newarr.length > 0 && (newarr[newarr.length-1] === pair || newarr[newarr.length-1] === item)) {
+      newarr.splice(-1, 1);
+    } else {
+      newarr.push(item);
+    }
+  }
+}
+
+return (newarr.length === 0);
+
 }
